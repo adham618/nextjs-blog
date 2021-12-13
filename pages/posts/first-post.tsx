a3bae998b6c57bafdf9109fe18e0ca2876a688f1
adham618 <adhamtarek291@gmail.com>
2021-12-13 21:06:31 +0200

    add layout

--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,6 +1,20 @@
-const FirstPost: React.FC  = () => {
+import Head from "next/head";
+import Link from "next/link";
+import Layout from "../../components/Layout";
+
+const FirstPost: React.FC = () => {
   return (
+    <Layout>
+      <Head>
+        <title>First Page</title>
+      </Head>
       <h1>First Post</h1>
-    )
-}
-export default FirstPost
+      <h2>
+        <Link href="/">
+          <a>Back to home</a>
+        </Link>
+      </h2>
+    </Layout>
+  );
+};
+export default FirstPost;
